bcce61a943ccfd648ef3f7261eba59efbcbbe77e
hanweidong1 <hanweidong@nec.cn>
2018-11-14 13:59:35 +0800

    asd

--- src/oa/oa.java
+++ src/oa/oa.java
@@ -3,6 +3,11 @@ package oa;
 public class oa {
 
 	 public static void main(String[] args) {
-		System.out.println("sss12333");
+		System.out.println("sss1233322323");
+		System.out.println("sss1233322323");
+		System.out.println("sss1233322323");
+		System.out.println("sss1233322323");
+		System.out.println("sss1233322323");
+		System.out.println("sss1233322323");
 	}
 }
