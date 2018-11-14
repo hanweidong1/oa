b94daef862b4bfe9cb634a0b7e57094d80da8add
hanweidong1 <hanweidong@nec.cn>
2018-11-14 13:41:55 +0800

    提交
    Change-Id: I4ddd484299501840fde824132506257fa54f4225
    Signed-off-by: hanweidong1 <hanweidong@nec.cn>

--- src/oa/oa.java
+++ src/oa/oa.java
@@ -3,6 +3,6 @@ package oa;
 public class oa {
 
 	 public static void main(String[] args) {
-		System.out.println("sss");
+		System.out.println("sss12333");
 	}
 }
---
+++ src/oa/test3.java
@@ -0,0 +1,9 @@
+package oa;
+
+public class test3 {
+ private void mian() {
+	// TODO Auto-generated method stub
+	 System.out.println("3");
+
+}
+}
